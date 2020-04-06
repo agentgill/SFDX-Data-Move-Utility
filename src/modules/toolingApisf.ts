d03d82434d80817939c993057068d9e3587d5814
hknokh <hknokh@salesforce.com>
2020-04-06 08:29:48 +0300

    Update

--- src/modules/toolingApisf.ts
+++ src/modules/toolingApisf.ts
@@ -1,9 +1,17 @@
-import  { DescribeSObjectResult, QueryResult } from 'jsforce';
-
+import { Connection } from 'jsforce';
+import * as SfdmModels from './models/index';
 
 
 export class ToolingApiSf {
 
+    sOrg: SfdmModels.SOrg;
+
+    constructor(sOrg : SfdmModels.SOrg) {
+        this.sOrg = sOrg;
+    }
+   
     
+
+
 
 }
